4b769bd2f794ef2b38ebf0fa662394e232fe5166
Marko <125321793+mara557@users.noreply.github.com>
2024-10-06 01:19:00 +0100

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.22" apply false
-}
-
-// Removed buildscript and allprojects blocks
